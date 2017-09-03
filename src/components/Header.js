0a964e8418b911e09719e0f389de33238685ef2e
Dory <dorybaga@gmail.com>
2017-09-02 14:09:36 -1000

    made header component; renders to browser

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div id="Header">
+      <div id="appName">
+        <h2>kanban</h2>
+      </div>
+      <div id="addNewTaskBox">
+        <button id="addNewTask">New Task</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// create the store
+// let store = createStore(
+
+// )
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
 registerServiceWorker();
